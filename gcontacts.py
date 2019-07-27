46465be1d8325ba8780967ab2ec1beb4e7882781
Mike Gering <mike@mfg-consulting-llc.com>
2019-07-27 16:38:56 -0400

    tabify

--- gcontacts.py
+++ gcontacts.py
@@ -6,26 +6,26 @@ import google_map, postbox_map
 # postbox_order: name => col_num
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('google', help="Google contacts in csv file input")
-  parser.add_argument('--postbox', help="Postbox csv file output")
-  args = parser.parse_args()
-  with open(args.google, newline='') as google_csvfile:
-    post_file = sys.stdout
-    if args.postbox:
-      post_file = open(args.postbox, "w", newline='')
-    #wrtr = csv.writer(post_file, delimiter=',', quotechar='"')
-    wrtr = csv.DictWriter(post_file, postbox_map.postbox_fields,)
-    wrtr.writeheader()
-    rdr = csv.reader(google_csvfile, delimiter=',', quotechar='"')
-    hdr = next(rdr)
-    for row in rdr:
-      google_contact = {}
-      for col_num in range(0, len(hdr)):
-        google_contact[hdr[col_num]] = row[col_num]
-      postbox_contact = google_map.handle_google_contact(google_contact)
-      wrtr.writerow(postbox_contact)
-    
+	parser = argparse.ArgumentParser()
+	parser.add_argument('google', help="Google contacts in csv file input")
+	parser.add_argument('--postbox', help="Postbox csv file output")
+	args = parser.parse_args()
+	with open(args.google, newline='') as google_csvfile:
+		post_file = sys.stdout
+		if args.postbox:
+			post_file = open(args.postbox, "w", newline='')
+		#wrtr = csv.writer(post_file, delimiter=',', quotechar='"')
+		wrtr = csv.DictWriter(post_file, postbox_map.postbox_fields,)
+		wrtr.writeheader()
+		rdr = csv.reader(google_csvfile, delimiter=',', quotechar='"')
+		hdr = next(rdr)
+		for row in rdr:
+			google_contact = {}
+			for col_num in range(0, len(hdr)):
+				google_contact[hdr[col_num]] = row[col_num]
+			postbox_contact = google_map.handle_google_contact(google_contact)
+			wrtr.writerow(postbox_contact)
+		
 
 if __name__ == '__main__':
-  main()
+	main()
